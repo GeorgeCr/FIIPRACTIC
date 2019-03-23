aed38698297e9f6000d771bf77802843fb918ec8
GeorgeCr <georgecraciun1998@gmail.com>
2019-03-23 13:09:18 +0200

    massive changes - added components services, etc.

---
+++ angular-fii/src/app/presentation/components/category/category-trainers/category-trainers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-category-trainers',
+  templateUrl: './category-trainers.component.html',
+  styleUrls: ['./category-trainers.component.scss']
+})
+export class CategoryTrainersComponent implements OnInit {
+  @Input() trainersInput;
+  @Input() ariaDescription;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
